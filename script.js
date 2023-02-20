d7a712d27963a5915dde9730e126924e1ff3c7dc
Arnab Dutta <108853322+arnabrock09@users.noreply.github.com>
2023-02-20 19:08:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Frontend Developer", "Gamer", "Designer"],
+        strings: ["YouTuber", "Web Developer", "App Developer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Gamer", "Designer"],
+        strings: ["YouTuber", "Web Developer", "App Developer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
